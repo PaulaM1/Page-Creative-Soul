42494acf5232355cddd85a4f9d02acf34961854d
Paula Mikszewicz <pmikszewicz@gmail.com>
2019-01-09 17:48:53 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// var Intro = document.querySelector(".nav_intro");
+// var Projects = document.querySelector(".nav_projects");
+// var About = document.querySelector(".nav_aboutme");
+// var Contact = document.querySelector(".nav_contact");
+
+
